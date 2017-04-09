a433a887a582974dc02f9789cfdcc13f69a6fc13
Alex Law <achlawuk@gmail.com>
2017-04-09 15:09:35 +0100

    Add 3 and 5 fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@
  	it 'returns "buzz" when passed 5' do
  		expect(fizzbuzz(5)).to eq 'buzz'
  	end
+ 	it 'returns "fizzbuzz" when passed 3 and 5' do
+ 		expect(fizzbuzz(15)).to eq 'fizzbuzz'
+ 	end
  end
